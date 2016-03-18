7ef5b06239f63607a9bf7b4711708af38014a2f9
Keith Philpott <keith@digitalbutter.com>
2016-03-18 12:48:50 -0700

    register count units

--- registry.py
+++ registry.py
@@ -1,4 +1,5 @@
 from .base import UnitDict
+from .count import count_units
 from .length import length_units
 from .mass import mass_units
 from .volume import volume_units
@@ -8,11 +9,10 @@ unit_registry = {}
 
 def register_units(units: UnitDict):
     for unit_key, unit in units.items():
-        assert unit_key not in unit_registry, \
-            "{} already registered".format(unit.name)
+        assert unit_key not in unit_registry, "{} already registered".format(unit.name)
 
         unit_registry[unit_key] = unit
 
 
-for unit_dict in (length_units, mass_units, volume_units):
+for unit_dict in (count_units, length_units, mass_units, volume_units):
     register_units(unit_dict)
