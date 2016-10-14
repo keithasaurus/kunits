3da9ac9c79b57bf7a0a1bcd9b3d8bdacccc198cf
Keith Philpott <keith@digitalbutter.com>
2016-10-14 13:33:15 -0700

    use frozendict for unit_registry

--- registry.py
+++ registry.py
@@ -4,18 +4,23 @@ from .length import length_units
 from .mass import mass_units
 from .time import time_units
 from .volume import volume_units
-
-unit_registry = {}  # type: UnitDict
-
-
-def register_units(units: UnitDict):
-    for unit_key, unit in units.items():
-        assert unit_key not in unit_registry, "{} already registered".format(
-            unit.name)
-
-        unit_registry[unit_key] = unit
+from frozendict import frozendict
 
 
-for unit_dict in (
-        count_units, length_units, mass_units, volume_units, time_units):
-    register_units(unit_dict)
+def register_all_units():
+    unit_registry = {}
+
+    def register_units(units: UnitDict):
+        for unit_key, unit in units.items():
+            assert unit_key not in unit_registry, "{} already registered".format(
+                unit.name)
+
+            unit_registry[unit_key] = unit
+
+    for unit_dict in [count_units, length_units, mass_units,
+                      volume_units, time_units]:
+        register_units(unit_dict)
+
+    return frozendict(unit_registry)
+
+unit_registry = register_all_units()  # type: UnitDict
