a186a2e588dbc2ca44ea64049111cb74e2fb3b88
Keith Philpott <keith@digitalbutter.com>
2017-01-02 12:38:31 -0800

    flake8 indentation fix

--- mass/metric.py
+++ mass/metric.py
@@ -8,8 +8,8 @@ gram_to_gram = StandardTransform(
 )
 
 units: UnitDict = {
-   unit.name: unit for unit
-   in generate_metric_units(name='gram',
-                            abbrev='g',
-                            standard_transform=gram_to_gram)
+    unit.name: unit for unit
+    in generate_metric_units(name='gram',
+                             abbrev='g',
+                             standard_transform=gram_to_gram)
 }
